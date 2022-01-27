f2e28bfc9d81fcd066fe8249f81bc4db94e9c778
seunguk1 <leeseunguk3@gmail.com>
2022-01-27 17:54:28 +0900

    한줄평점 등록 및 디비연결

--- src/main/java/com/jhta/moviefan/dao/CommentDao.java
+++ src/main/java/com/jhta/moviefan/dao/CommentDao.java
@@ -12,6 +12,8 @@ public interface CommentDao {
 	void insertComment(Comment comment);
 	void updateCommentByNo(Comment comment);
 	void deleteCommentByNo(Comment comment);
+	List<Comment> getMovieTitleByMovieNo(int movieNo);
+	List<Comment> getCustomerIdBycustomerNo(int customerNo);
 	List<Comment> getAllComments();
 	List<Comment> searchCommentsByMovieNo(int movieNo);
 	List<Comment> searchCommnetsByCustomerNo(int customerNo);
