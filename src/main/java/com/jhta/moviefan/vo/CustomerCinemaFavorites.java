901226e9826f2dba78e775ad5e7ab0623ad0957c
SungGeun Park <sg2pooh@gmail.com>
2022-02-07 18:15:38 +0900

    극장 찜하기 추가 중

---
+++ src/main/java/com/jhta/moviefan/vo/CustomerCinemaFavorites.java
@@ -0,0 +1,34 @@
+package com.jhta.moviefan.vo;
+
+public class CustomerCinemaFavorites {
+	
+	private int customerNo;
+	private int cinemaNo;
+	
+	CustomerCinemaFavorites() {}
+
+	public int getCustomerNo() {
+		return customerNo;
+	}
+
+	public void setCustomerNo(int customerNo) {
+		this.customerNo = customerNo;
+	}
+
+	public int getCinemaNo() {
+		return cinemaNo;
+	}
+
+	public void setCinemaNo(int cinemaNo) {
+		this.cinemaNo = cinemaNo;
+	}
+
+	@Override
+	public String toString() {
+		return "CustomerCinemaFavorites [customerNo=" + customerNo + ", cinemaNo=" + cinemaNo + "]";
+	}
+
+	
+	
+
+}
