911b3795fe05a4e1129046c86d6a0f2d052d61d4
DESKTOP-DRVCKLU\daye <ekdpoa@gmail.com>
2022-02-10 18:01:42 +0900

    관리자 - 상영정보 리스트 페이징처리

--- src/main/java/com/jhta/moviefan/controller/CinemaController.java
+++ src/main/java/com/jhta/moviefan/controller/CinemaController.java
@@ -2,6 +2,7 @@ package com.jhta.moviefan.controller;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/com/jhta/moviefan/restcontroller/CinemaRestController.java
+++ src/main/java/com/jhta/moviefan/restcontroller/CinemaRestController.java
@@ -2,6 +2,7 @@ package com.jhta.moviefan.restcontroller;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
